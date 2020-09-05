2be4720532f1b1d86c767439d7dc7300d5c098d2
Grekus14 <georgepopovich14@gmail.com>
2020-09-05 12:49:39 +0200

    Develop part of the checkout page

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -3,13 +3,14 @@ import Homepage from './Homepage';
 import ProductPage from './ProductPage';
 import LoginPage from './LoginPage';
 import AdminPage from './AdminPage';
+import CheckoutPage from './CheckoutPage';
 import Navbar from './partial/Navbar';
 
 function App() {
 	return (
 		<>
 			<Navbar />
-			<Homepage />
+			<CheckoutPage />
 		</>
 	);
 }

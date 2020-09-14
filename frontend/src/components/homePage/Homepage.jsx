efd593935af20c3a467b8f1a7a26a2c05d3f222c
Grekus14 <georgepopovich14@gmail.com>
2020-09-14 18:58:14 +0200

    Add pagination to homepage

--- frontend/src/components/homePage/Homepage.jsx
+++ frontend/src/components/homePage/Homepage.jsx
@@ -14,13 +14,16 @@ function Homepage() {
 	const userRole = useSelector(state => state.userRole);
 	const editPanelState = useSelector(state => state.editPanelState);
 	const queries = useSelector(state => state.queries);
+	const totalPages = products.pages || null;
 
 	const [query, setQuery] = useState(queries.query || '');
 	const [minPrice, setMinPrice] = useState(queries.minPrice || '');
 	const [maxPrice, setMaxPrice] = useState(queries.maxPrice || '');
 	const [category, setCategory] = useState(queries.category || '');
 	const [page, setPage] = useState(queries.page || 1);
-	const [totalPages, setTotalPages] = useState(null);
+
+	const [isLoading, setIsLoading] = useState(false);
+	const [preventFirstRender, setPreventFirstRender] = useState(true);
 
 	const pages = [];
 	for (let i = 0; i < totalPages; i++) {
@@ -34,39 +37,21 @@ function Homepage() {
 	}
 
 	useEffect(() => {
-		const source = axios.CancelToken.source();
-		const itemsPerPage = 1;
-
-		dispatch(setProducts('loading'));
-
-		async function getProducts() {
-			// get products
-			try {
-				const result = await axiosApp.get(
-					`/products/?itemsPerPage=${itemsPerPage}&query=${query}&minPrice=${minPrice}&maxPrice=${maxPrice}&category=${category}&page=${page}`,
-					{
-						cancelToken: source.token
-					}
-				);
-
-				console.log(result.data);
-				dispatch(setProducts(result.data.results));
-				setTotalPages(result.data.pages);
-			} catch (err) {
-				dispatch(setProducts([]));
-				console.log(err);
-			}
+		if (preventFirstRender && products !== 'loading') {
+			setPreventFirstRender(false);
+			return;
 		}
 
-		getProducts();
+		const source = axios.CancelToken.source();
+
+		getProducts(source.token);
 
 		return () => {
 			source.cancel();
 		};
-	}, [query, minPrice, maxPrice, category, editPanelState, page]);
+	}, [editPanelState, page]);
 
 	useEffect(() => {
-		console.log('called dispatcj');
 		dispatch(
 			setQueries({
 				query,
@@ -78,6 +63,27 @@ function Homepage() {
 		);
 	}, [query, minPrice, maxPrice, category, page]);
 
+	async function getProducts(token) {
+		setIsLoading(true);
+
+		const itemsPerPage = 4;
+
+		try {
+			const result = await axiosApp.get(
+				`/products/?itemsPerPage=${itemsPerPage}&query=${query}&minPrice=${minPrice}&maxPrice=${maxPrice}&category=${category}&page=${page}`,
+				{
+					cancelToken: token
+				}
+			);
+
+			dispatch(setProducts(result.data));
+		} catch (err) {
+			console.log(err);
+		}
+
+		setIsLoading(false);
+	}
+
 	function setPageFilter(value) {
 		setPage(value > totalPages ? totalPages : value || 1);
 	}
@@ -87,68 +93,82 @@ function Homepage() {
 			<Navbar />
 			<EditProductPanel />
 			<div className="products-main-container">
-				<div className="search-filters">
-					<div className="search">
-						<input
-							type="text"
-							placeholder="Search"
-							value={query}
-							onChange={e => setQuery(e.target.value)}
-						/>
-						<button className="btn-primary">SEARCH</button>
-					</div>
-					<div className="filters">
-						<span className="price-filters">
-							<span>
-								<p>From:</p>
-								<input
-									type="number"
-									placeholder="$$$"
-									value={minPrice / 100 || ''}
-									onChange={e =>
-										setMinPrice(
-											e.target.value
-												? e.target.value * 100
-												: ''
-										)
-									}
-								/>
+				<form
+					onSubmit={e => {
+						e.preventDefault();
+						getProducts();
+					}}>
+					<div className="search-filters">
+						<div className="search">
+							<input
+								type="text"
+								placeholder="Search"
+								value={query}
+								onChange={e => setQuery(e.target.value)}
+							/>
+							<button
+								type="submit"
+								className={
+									'btn-primary' +
+									(isLoading ? ' btn-primary-loading' : '')
+								}>
+								SEARCH
+							</button>
+						</div>
+						<div className="filters">
+							<span className="price-filters">
+								<span>
+									<p>From:</p>
+									<input
+										type="number"
+										placeholder="$$$"
+										value={minPrice / 100 || ''}
+										onChange={e =>
+											setMinPrice(
+												e.target.value
+													? e.target.value * 100
+													: ''
+											)
+										}
+									/>
+								</span>
+								<span>
+									<p>To:</p>
+									<input
+										type="number"
+										placeholder="$$$"
+										value={maxPrice / 100 || ''}
+										onChange={e =>
+											setMaxPrice(
+												e.target.value
+													? e.target.value * 100
+													: ''
+											)
+										}
+									/>
+								</span>
 							</span>
-							<span>
-								<p>To:</p>
-								<input
-									type="number"
-									placeholder="$$$"
-									value={maxPrice / 100 || ''}
-									onChange={e =>
-										setMaxPrice(
-											e.target.value
-												? e.target.value * 100
-												: ''
-										)
-									}
-								/>
-							</span>
-						</span>
 
-						<select
-							onChange={e => setCategory(e.target.value)}
-							className="btn-category">
-							<option value="">All Categories</option>
-							<option value="running">Running</option>
-							<option value="lifestyle">Lifestyle</option>
-							<option value="basketball">Basketball</option>
-						</select>
+							<select
+								onChange={e => setCategory(e.target.value)}
+								className="btn-category"
+								value={category}>
+								<option value="">All Categories</option>
+								<option value="running">Running</option>
+								<option value="lifestyle">Lifestyle</option>
+								<option value="basketball">Basketball</option>
+							</select>
+						</div>
 					</div>
-				</div>
-				{/* "https://images.nike.com/is/image/DotCom/315115_112?$NIKE_PWP_GRAY$&wid=600&hei=700" */}
+				</form>
 				<div className="products">
+					{!isLoading ? null : <div className="loading-products"></div>}
 					{products === 'loading' ? (
-						<h2>Loading...</h2>
-					) : products.length === 0 ? (
+						<h1>Loading...</h1>
+					) : products.results.length === 0 ? (
 						<h1>No products</h1>
 					) : (
-						products.map(product => (
+						products.results.map(product => (
 							<Product
 								product={product}
 								userRole={userRole}
